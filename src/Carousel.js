1c6f9d830c5e49c02e8491d760fa4df5db5992ea
Alexander Zabiela <alexanderzabiela@gmail.com>
2019-11-28 11:27:36 +0000

    configured slick slider

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import data from "./data";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 class Carousel extends Component {
   constructor() {
@@ -20,34 +23,45 @@ class Carousel extends Component {
   }
 
   render() {
+    const sliderSettings = {
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      swipeToSlide: true,
+      dots: true
+    };
+
     let products = this.state.products;
     return (
       <div className="container">
         <h2>Related Products</h2>
-        {products.length === 0 ? (
-          <h1>No Products</h1>
-        ) : (
-          products.map(product => (
-            <div key={product.name}>
-              <a href={product.url}>
-                <img
-                  src={product.productImageUrl}
-                  alt={product.productImageAltText}
-                />
-              </a>
-              <a href={product.url}>
-                <h3>{product.name}</h3>
-              </a>
-              <span>({product.salesUnit})</span>
-              <p>
-                {product.price.currency} {product.price.formattedValue}
-              </p>
-              <a href="#">
-                <button>+ Add to cart</button>
-              </a>
-            </div>
-          ))
-        )}
+        <Slider {...sliderSettings}>
+          {products.length === 0 ? (
+            <h1>No Products</h1>
+          ) : (
+            products.map(product => (
+              <div key={product.name}>
+                <a href={product.url}>
+                  <img
+                    src={product.productImageUrl}
+                    alt={product.productImageAltText}
+                  />
+                </a>
+                <a href={product.url}>
+                  <h3>{product.name}</h3>
+                </a>
+                <span>({product.salesUnit})</span>
+                <p>
+                  {product.price.currency} {product.price.formattedValue}
+                </p>
+                <a href="#">
+                  <button>+ Add to cart</button>
+                </a>
+              </div>
+            ))
+          )}
+        </Slider>
       </div>
     );
   }
