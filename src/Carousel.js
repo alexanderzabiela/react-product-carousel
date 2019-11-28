0ad7be391929cc0a70dd3857c0f83bed51fef1c3
Alexander Zabiela <alexanderzabiela@gmail.com>
2019-11-28 11:04:10 +0000

    set up dependancies, scripts, linting and made skeleton

---
+++ src/Carousel.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Carousel extends Component {
+  render() {
+    return (
+      <p>placeholder</p>
+      //fetch data from json file
+      //map throught and store in state
+      //set up slick slider
+      //display relevant info for each product (image, name, sales unit and price )
+    );
+  }
+}
+
+export default Carousel;
